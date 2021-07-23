9113e4f2a954a86b6180451bf8d70642d1a72e6f
SandraT <sndrbltr@gmail.com>
2021-07-23 15:36:20 +0000

    Fixed sales start

--- index.js
+++ index.js
@@ -10,38 +10,49 @@ const trade = new Sales('Oak furniture', 'EUR');
 trade.intro();
 // It's an one time sales activity called "Oak furniture".
 
-trade.addItem('oak table', 150);
-// Oak table cost is 150 EUR!
+trade.addItem('oak table', 1, 150, 200);
+// Bought 1pc oak table for cost 150 EUR and plans to sell is for 200 EUR. 
 
-trade.addItem('oak bench', 110);
-// Oak bench cost is 110 EUR!
+trade.addItem('oak bench', 2, 110, 160);
+// Bought 2pcs oak wardrobe for cost 220 EUR and plans to sell is for 320 EUR. 
 
-trade.addItem('oak chair', 50);
-// Oak chair cost is 50 EUR!
+trade.addItem('oak chair', 4, 50, 80);
+// Bought 4pcs oak chair for cost 200 EUR and plans to sell is for 320 EUR. 
 
-trade.addItem('oak cabinet', 200);
-// Oak cabinet for 200 EUR!
+trade.addItem('oak cabinet', 2, 200, 280);
+// Bought 2pcs oak cabinet for cost 400 EUR and plans to sell is for 560 EUR. 
 
-trade.addItem('oak wardrobe', 300);
-// Oak wardrobe cost is 300 EUR!
+trade.addItem('oak wardrobe', 1, 300, 400);
+// Bought 1pc oak wardrobe for cost 300 EUR and plans to sell is for 400 EUR. 
 
-trade.qty();
+trade.itemsBought();
+// Total items bought by "Oak furniture":
+// ====================
+// 1) oak table - 1pc for 150 EUR;
+// 2) oak bench - 2pcs for 220 EUR;
+// 3) oak chair - 4pcs for 200 EUR;
+// 4) oak cabinet - 2pcs for 400 EUR;
+// 5) oak wardrobe - 1pc for 300 EUR;
+// ====================
 
-trade.sellItem(1, 200);
-// "Oak furniture" sells oak table for 200 EUR!
+trade.itemsForSales();
+// "Oak furniture" offers:
+// ====================
+// 1) oak table - 1pc for 200 EUR/pc;
+// 2) oak bench - 2pcs for 160 EUR/pc;
+// 3) oak chair - 4pcs for 50 EUR/pc;
+// 4) oak cabinet - 2pcs for 400 EUR/pc;
+// 5) oak wardrobe - 1pc for 300 EUR/pc;
+// ====================
+ 
+trade.totalCost();
+// "Oak furniture" bought furniture for 1270 EUR totally!
 
-trade.sellItem(1, 160);
-// "Oak furniture" sells oak bench for 160 EUR!
+trade.totalProfit();
+// "Oak furniture" profit is 530 EUR totally!
 
-trade.sellItem(1, 80);
-// "Oak furniture" sells oak chair for 80 EUR!
-
-trade.sellItem(1, 280);
-// "Oak furniture" sells oak cabinet for 280 EUR!
-
-trade.sellItem(1, 400);
-// "Oak furniture" sells oak wardrobe for 400 EUR!
-
+trade.surchargeAverage();
+// Average of total surcharge is 53 EUR!
 
 
 

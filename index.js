9ec947ce90b7aa347286b580cd72eefbd127c5d4
SandraT <sndrbltr@gmail.com>
2021-07-23 14:21:43 +0000

    Sales start

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
 // čia dėti visą pavyzdinį kodą, kuris turėtų paaiškinti, 
 //kaip naudotis jūsų sukurtu kodu
+
+//const itemsData = require('./itemsData');
+const Sales = require('./Sales');
+
+const trade = new Sales('Oak furniture', 'EUR');
+//const trade = new Sales(itemsData);
+
+trade.intro();
+// It's an one time sales activity called "Oak furniture".
+
+trade.addItem('oak table', 150);
+// Oak table cost is 150 EUR!
+
+trade.addItem('oak bench', 110);
+// Oak bench cost is 110 EUR!
+
+trade.addItem('oak chair', 50);
+// Oak chair cost is 50 EUR!
+
+trade.addItem('oak cabinet', 200);
+// Oak cabinet for 200 EUR!
+
+trade.addItem('oak wardrobe', 300);
+// Oak wardrobe cost is 300 EUR!
+
+trade.qty();
+
+trade.sellItem(1, 200);
+// "Oak furniture" sells oak table for 200 EUR!
+
+trade.sellItem(1, 160);
+// "Oak furniture" sells oak bench for 160 EUR!
+
+trade.sellItem(1, 80);
+// "Oak furniture" sells oak chair for 80 EUR!
+
+trade.sellItem(1, 280);
+// "Oak furniture" sells oak cabinet for 280 EUR!
+
+trade.sellItem(1, 400);
+// "Oak furniture" sells oak wardrobe for 400 EUR!
+
+
+
+
+
---
+++ itemsData.js
@@ -0,0 +1,39 @@
+const itemsData = {
+    name: 'Single Trade',
+    brand: 'Oak furniture',
+    curr: 'EUR',
+    itemInfo: [
+        {
+            item: 'table',
+            cost: 150,
+            price: 200,
+            qty: 1,
+        },
+        {
+            item: 'bench',
+            cost: 110,
+            price: 160,
+            qty: 2,
+        },
+        {
+            item: 'chair',
+            cost: 50,
+            price: 80,
+            qty: 4,
+        },
+        {
+            item: 'cabinet',
+            cost: 200,
+            price: 280,
+            qty: 2,
+        },
+        {
+            item: 'wardrobe',
+            cost: 300,
+            price: 400,
+            qty: 1,
+        }
+    ],
+}
+
+module.exports = itemsData;
